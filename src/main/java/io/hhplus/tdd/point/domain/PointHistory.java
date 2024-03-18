a9e227cb98715fda595139eb58cb00bcbf306865
gleys <hse6042@naver.com>
2024-03-19 00:42:21 +0900

    feat: 포인트 내역 생성 검증 구현

--- src/main/java/io/hhplus/tdd/point/domain/PointHistory.java
+++ src/main/java/io/hhplus/tdd/point/domain/PointHistory.java
@@ -8,4 +8,19 @@ public record PointHistory(
     Long timeMillis
 ) {
 
+    public PointHistory(final Long id, final Long userId, final TransactionType type, final Long amount, final Long timeMillis) {
+        if (isAmountMinus(amount)) {
+            throw new IllegalArgumentException();
+        }
+        this.id = id;
+        this.userId = userId;
+        this.type = type;
+        this.amount = amount;
+        this.timeMillis = timeMillis;
+    }
+
+    private boolean isAmountMinus(final Long amount) {
+        return amount < 0;
+    }
+
 }
--- src/test/java/io/hhplus/tdd/point/domain/PointHistoryTest.java
+++ src/test/java/io/hhplus/tdd/point/domain/PointHistoryTest.java
@@ -1,7 +1,25 @@
 package io.hhplus.tdd.point.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointHistoryTest {
+
+    @Test
+    void 타입에_상관없이_금액이_음수일_때_예외가_발생_한다() {
+        //given
+        Long paymentTotal = -10L;
+
+        //when & then
+        Assertions.assertThatThrownBy(() -> new PointHistory(
+                1L, 1L, TransactionType.CHARGE, paymentTotal, 1L))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        Assertions.assertThatThrownBy(() -> new PointHistory(
+                        1L, 1L, TransactionType.USE, paymentTotal, 1L))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
